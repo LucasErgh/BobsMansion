91e2f59b89a7f574bb9c08413a566cac02f4c112
Lucas Ergh <lucasergh@gmail.com>
2025-07-17 18:20:37 -0400

    Add bullets and collisions to gun

--- main.cpp
+++ main.cpp
@@ -3,38 +3,8 @@
 #include <string>
 #include <cmath>
 #include <cstring>
-
-void NormalizeAngle(float& angle) {
-    if (angle > PI)
-        angle = angle-2*PI;
-    if (angle < -PI)
-        angle = angle+2*PI;
-}
-
-Matrix getBobsRotation(float& BobAngle, const Camera camera, const Vector3& BobPosition){
-
-    float verticalRotation = atan2(BobPosition.y - camera.position.y, sqrt( (camera.position.x - BobPosition.x)*(camera.position.x - BobPosition.x) + (camera.position.y - BobPosition.y)*(camera.position.y - BobPosition.y)));
-
-    const float tolerance = DEG2RAD*2;
-    const float degreesPerFrame = 1;
-    const float turnRate = DEG2RAD*degreesPerFrame;
-
-    auto AngleBetween = atan2( (camera.position.x - BobPosition.x), (camera.position.z - BobPosition.z));
-    AngleBetween -= PI/4;
-
-    NormalizeAngle(AngleBetween);
-
-    float diff = std::abs(AngleBetween-BobAngle);
-
-    if (AngleBetween > (BobAngle + tolerance) && diff < PI || AngleBetween < (BobAngle + tolerance) && diff > PI)
-        BobAngle += turnRate;
-    else if (AngleBetween > (BobAngle - tolerance) && diff > PI || AngleBetween < (BobAngle - tolerance) && diff < PI)
-        BobAngle -= turnRate;
-
-    NormalizeAngle(BobAngle);
-
-    return MatrixRotateXYZ( { 0, BobAngle, 0});
-}
+#include <vector>
+#include <iostream>
 
 void mainMenu(Camera& camera, const int screenHeight){
     while(!WindowShouldClose()){
@@ -51,26 +21,53 @@ void mainMenu(Camera& camera, const int screenHeight){
     }
 }
 
+void moveBullets(std::vector<std::pair<Vector3, Vector3>>& bullets, BoundingBox& bounds, std::vector<BoundingBox>& hitboxes, Sound& hit){
+    const float velocityScalar = 2.0f;
+    auto cur = bullets.begin();
+    while(cur != bullets.end()){
+        if (cur->first.x > bounds.max.x || cur->first.y > bounds.max.y || cur->first.z > bounds.max.z
+            || cur->first.x < bounds.min.x || cur->first.y < bounds.min.y || cur->first.z < bounds.min.z)
+        {
+            bullets.erase(cur);
+        }
+        else {
+            Ray ray;
+            Vector3 startPos = cur->first;
+            ray.position = cur->first;
+            cur->first = Vector3Add(cur->first, Vector3Scale(cur->second, velocityScalar));
+            ray.direction = Vector3Subtract(cur->first, ray.position);
+            bool collision = false;
+            for (auto& box : hitboxes){
+                auto col = GetRayCollisionBox(ray, box);
+                if (col.hit && Vector3Distance(col.point, startPos) < Vector3Distance(cur->first, startPos)){
+                    std::cout << "Collision Distance: " << Vector3Distance(col.point, startPos) << ", Bullet Travel: " << Vector3Distance(cur->first, startPos) << '\n';
+                    PlaySound(hit);
+                    bullets.erase(cur);
+                    collision = true;
+                }
+            }
+            if (!collision)
+                ++cur;
+        }
+    }
+}
+
 int main(void){
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+const int screenWidth = 1920;
+const int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight, "Bob's Mansion");
-    InitAudioDevice();
+InitWindow(screenWidth, screenHeight, "Bob's Mansion");
+InitAudioDevice();
 
-    Camera camera = { 0 };
+Camera camera = { 0 };
 
     mainMenu(camera, screenHeight);
 
     camera.position = (Vector3){ 0.0f, 1.0f, 5.0f };
     camera.target = (Vector3){ 3.0f, 1.0f, 3.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
+    camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
-
-    Model Martian = LoadModel("../assets/MartianBustV3.glb");
-    Model BobModel = LoadModel("../assets/BobModel.glb"); 
-    Vector3 MartianPosition = { -25.0f, 7.0f, 0.0f};
 
     Image GunImage = LoadImage("../assets/Gun.png");
     Image GunFiringImage = LoadImage("../assets/GunFiring.png");
@@ -80,9 +77,8 @@ int main(void){
     Texture2D Melee = LoadTextureFromImage(MeleeImage);
     Rectangle GunDrawLocation = { (screenWidth+750)/2, screenHeight-750, screenWidth, screenHeight};
     Rectangle MeleeDrawLocation = { screenWidth/2, screenHeight-750, screenWidth/2+750, screenHeight};
+    BoundingBox bounds { {-100, -100, -100}, {100, 100, 100} };
 
-    float BobAngle = -PI/4;
-    Vector3 BobPosition = { 3.0f, 0.5f, 3.0f };
 
     const int numPews = 3;
     int PewIndex = 0;
@@ -90,13 +86,39 @@ int main(void){
     int isShooting = 0;
     int isMeleeing = 0;
 
+    Mesh cubeMesh = GenMeshCube(1.0f, 2.0f, 2.0f);
+    Model cube = LoadModelFromMesh(cubeMesh);
+
+    Mesh sphereMesh = GenMeshSphere(0.4f, 16, 16);
+    Model sphere = LoadModelFromMesh(sphereMesh);
+
+    std::vector<std::pair<Vector3, Vector3>> bullets = {};
+    Sound hit = LoadSound("../assets/hit.wav");
+
+    std::vector<BoundingBox> hitboxes;
+    hitboxes.push_back(GetMeshBoundingBox(cube.meshes[0]));
+
+    Vector3 spherePos;
+    Vector3 sphereDir;
+
     DisableCursor();
     SetTargetFPS(30);
+
+    float dt = 0;
 
     while(!WindowShouldClose()){
         UpdateCamera(&camera, CAMERA_FIRST_PERSON);
 
+        moveBullets(bullets, bounds, hitboxes, hit);
+
+        dt += GetFrameTime();
+        cube.transform = MatrixRotateX(dt/10.0f);
+
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !isShooting && !isMeleeing) {
+            spherePos = camera.position;
+            sphereDir = Vector3Subtract(camera.target, spherePos);
+            bullets.push_back(std::make_pair(spherePos, sphereDir));
+
             isShooting = 5;
             PlaySound(Pews[PewIndex]);
             ++PewIndex;
@@ -114,14 +136,16 @@ int main(void){
 
         BeginDrawing();
             ClearBackground(BLACK);
-
-            BobModel.transform = getBobsRotation(BobAngle, camera, BobPosition);
+            DrawText(std::to_string(dt).c_str(), 10, 10, 20, WHITE);
+            DrawText(("CamTarget: " + std::to_string(camera.target.x) + std::to_string(camera.target.y) + std::to_string(camera.target.z)).c_str(), 10, 20, 20, WHITE);
 
             BeginMode3D(camera);
 
-                DrawModel(Martian, MartianPosition, 4.0f, WHITE);
-                DrawModel(BobModel, BobPosition, 1.0f, WHITE);
+                DrawModelWires(cube, {0, 1, 0}, 1.0f, BLUE);
                 DrawPlane( {0, 0, 0}, {20, 20}, GRAY);
+                for (auto& cur: bullets){
+                    DrawModel(sphere, cur.first, 1.0f, GREEN);
+                }
 
             EndMode3D();
 
@@ -141,3 +165,5 @@ int main(void){
         EndDrawing();
     }
 }
+
+
